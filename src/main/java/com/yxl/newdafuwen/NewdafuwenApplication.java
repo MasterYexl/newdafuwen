073110264d00826a25f10c7382b4d4b6ce46700b
叶向林 <yexianglin@outlook.com>
2022-12-29 22:54:26 +0800

    init application

---
+++ src/main/java/com/yxl/newdafuwen/NewdafuwenApplication.java
@@ -0,0 +1,13 @@
+package com.yxl.newdafuwen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewdafuwenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewdafuwenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yxl/newdafuwen/NewdafuwenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yxl.newdafuwen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewdafuwenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
